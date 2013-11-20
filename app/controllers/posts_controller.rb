cd5d695399d2d0d5d4ab64a03d368086a2469e5c
tyler-rand <tprand@syr.edu>
2013-11-20 16:05:52 -0500

    some css fixed, add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
 
+	def index
+		@posts = Post.all
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.create(post_params)
+		respond_to do |format|
+			if @post.save
+				format.html { redirect_to posts_path }
+			else
+				format.html { redirect_to new_post_path }
+			end
+		end
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
 	def post_params
 		params.require(:post).permit(:title, :body)
 	end
